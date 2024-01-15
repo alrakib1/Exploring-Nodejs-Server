918f15d8d4b51e9040accc9352ebbe7ec26a77a1
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-01-15 20:14:51 +0600

    learned node js server createion

--- node js file read write, callback, sync, async/index.js
+++ node js file read write, callback, sync, async/index.js
@@ -1,25 +1,47 @@
 const fs = require("fs");
 
+const http = require('http');
+
+
+
+// ////////////////////////////////////////////////////
+
+// Files
+
 //  Blocking sync way
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-// console.log(textIn);
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// // console.log(textIn);
 
-const textOut = `This is what we know : ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
+// const textOut = `This is what we know : ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
 
-// console.log(textOut);
+// // console.log(textOut);
 
-// Non-blocking, asynchronous way
+// // Non-blocking, asynchronous way
 
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-      console.log(data3);
-      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-        console.log("file has been written :v");
-      });
-    });
-  });
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("file has been written :v");
+//       });
+//     });
+//   });
+// });
+
+
+//////////////////////////////////////////
+
+// Server
+
+
+const server = http.createServer((req,res)=>{
+  res.end('Hello from node server!')
 });
+
+
+
+
